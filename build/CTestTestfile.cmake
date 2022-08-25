a0863821238437169bf765b224472776bad2b7a4
akshatowl <akshatpandeyplus41@gmail.com>
2022-08-25 13:08:17 -0400

    moveit packages

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/akshat/pick-and-place/src
+# Build directory: /home/akshat/pick-and-place/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("nodes")
+subdirs("rarm_description")
